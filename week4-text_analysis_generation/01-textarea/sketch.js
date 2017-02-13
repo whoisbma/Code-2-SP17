83f6f17768def111adf54a90a9ca7dae3dc6882f
Bryan Ma <bryanhma@gmail.com>
2017-02-13 16:10:20 -0500

    4

--- week4-text_analysis_generation/01-textarea/sketch.js
+++ week4-text_analysis_generation/01-textarea/sketch.js
@@ -1,19 +1,33 @@
-var button;
-var textarea;
-var savedText;
+var button;     // variable to store reference to the button HTML element
+var textarea;   // variable to store reference to the textarea HTML element
+var savedText;  // variable to store the string from the textarea
 
 function setup() {
   noCanvas();
   
+  // create a textarea HTML element with some filler text
   textarea = createElement('textarea', 'Enter text here!');
+  // access the size parameter of the textarea element.
+  // remember all of this could be done in simple HTML too.
   textarea.size(300,400);
 
+  // create a <br> HTML element to space things out
   createElement('br');
+
+  // create a button and use the button variable to store a reference to it
   button = createButton('push to save');
   
+  // register callback saveText to the mousePressed event of the button HTML element
   button.mousePressed(saveText);
+
+  // this could instead be written with an anonymous callback like so:
+  // button.mousePressed(function() {
+  //   createP(textarea.value());
+  // });
 }
 
 function saveText() {
-  createP(textarea.value());
+  // save the value of the textarea to the savedText variable and create a new HTML paragraph element to display it.
+  savedText = textarea.value();
+  createP(savedText);
 }
