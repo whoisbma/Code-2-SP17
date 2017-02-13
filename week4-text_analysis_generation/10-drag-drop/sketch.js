9c3f8addf90b3d7b8794bc6d4fc12f7255078818
Bryan Ma <bryanhma@gmail.com>
2017-02-13 18:26:19 -0500

    4

--- week4-text_analysis_generation/10-drag-drop/sketch.js
+++ week4-text_analysis_generation/10-drag-drop/sketch.js
@@ -1,45 +1,36 @@
 var dropzone;
-var canvas;
+// var canvas;
 var img;
 var theText;
 
 function setup() {
-  canvas = createCanvas(400,400);
+  // canvas = createCanvas(400,400);
   background(255);
   
   // the index.html file has a div with an ID of 'dropzone'.
   // use select() to assign a reference to it in the variable dropzone
   dropzone = select('#dropzone');
 
-  // register callbacks for the dropzone on events dragOver(), dragLeave(), and drop()
-  dropzone.dragOver(highlight);
-  dropzone.dragLeave(unhighlight);
-  dropzone.drop(gotFile); //first arg:  process/override default browser behavior, second arg: drop moment
-}
-
-function highlight() {
-  this.style('background-color', '#ccc');
-}
-
-function unhighlight() {
-  this.style('background-color', '#fff');
+  // register callback for the dropzone on drop() event
+  // this callback could also be anonymously declared
+  dropzone.drop(gotFile);
 }
 
 function gotFile(file) {
-  unhighlight();
-  console.log(file);
-  if (file != undefined) {
-    createP(file.name + ", " + file.type + ", " + file.size + " bytes");
-    if (file.type == 'image') {
-      createP(file.data);
-      img = createImg(file.data);
-      canvas.size(img.width, img.height);
-      image(img,0,0,width,height); //draw to canvas also
-    }
-    if (file.type == 'text') {
-      theText = file.data;
-      createP(file.data);
-    }
-  }
+  createP(file.name + ", " + file.type + ", " + file.size + " bytes");
+  
+  //check what kind of file it is, then do something based on if its an image or text
+  if (file.type == 'image') {
+    createP(file.data);
+    img = createImg(file.data);
 
+    // you could also draw the image to the canvas if you like
+    // make sure to uncomment the canvas creation in setup()
+    // canvas.size(img.width, img.height);
+    // image(img,0,0,width,height); 
+  }
+  if (file.type == 'text') {
+    theText = file.data;
+    createP(file.data);
+  }
 }
